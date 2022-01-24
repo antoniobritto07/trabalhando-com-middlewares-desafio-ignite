39c0a0d87b16e7686f294ed48de7174583bacf8a
Antônio Britto <antonio.britto.2000@hotmail.com>
2022-01-24 18:56:30 -0300

    feat: implementation of new middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,60 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404);
+  }
+  request.user = user
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if ((user.pro === false && (user.todos.length + 1) < 10) || (user.pro === true)) {
+    return next();
+  }
+  return response.status(403)
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404)
+  }
+
+  if (!validate(id)) {
+    return response.status(400)
+  }
+
+  const todoMatched = user.todos.find(todo => todo.id === id);
+
+  if (!todoMatched) {
+    return response.status(404).json({error: "Todo wasn't found!"})
+  }
+
+  request.todo = todoMatched;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404);
+  }
+  request.user = user
+  return next();
 }
 
 app.post('/users', (request, response) => {
